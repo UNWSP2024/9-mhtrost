bab9c356325bd1a466cf5c6296cd50f77127ddfc
mhtrost <mhtrost@students.unwsp.edu>
2026-03-23 15:15:26 -0500

    Assignment 9

--- program_1.py
+++ program_1.py
@@ -1,11 +1,14 @@
 #Micah Trost
 #March 23, 2026
-#Item Counter
+#Name Counter
 
 def count_file_lines():
-    open_file = open('names.txt', 'r')
-    print('The number of names in the file is:', len(open_file.readlines()))
-    print('In the count_file_lines function')
+    try:
+        with open('names.txt', 'r') as file:
+            line_count = sum(1 for line in file)
+            print(f'The number of names in the file is: {line_count}')
+    except IOError:
+        print("An error occurred while trying to read the file.")
 
 if __name__ == '__main__':
     count_file_lines()
