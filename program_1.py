42bdf21ad1ba5c6ad815a0e8fc9ea2ff4528e165
mhtrost <mhtrost@students.unwsp.edu>
2026-03-23 15:12:32 -0500

    Assignment 9

--- program_1.py
+++ program_1.py
@@ -1,16 +1,11 @@
-# Program #1: Item Counter
-# Assume a file containing a series of names (as strings) is named names.txt 
-# (Use the included example file names.txt) and exists on the computer's disk.
-# Write a program that displays the number of names that are stored in the file.
+#Micah Trost
+#March 23, 2026
+#Item Counter
 
 def count_file_lines():
-    ######################
-    # Add your code here #
-    ######################
+    open_file = open('names.txt', 'r')
+    print('The number of names in the file is:', len(open_file.readlines()))
     print('In the count_file_lines function')
 
-
-
-# You don't need to change anything below this line:
 if __name__ == '__main__':
     count_file_lines()
